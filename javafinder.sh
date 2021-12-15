948046e249d1c6515b3d9641fa582474e2763755
Farid Joubbi <farid@joubbi.se>
2021-12-15 09:30:02 +0100

    A little nicer report.

--- javafinder.sh
+++ javafinder.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 #########################################################################################################
-# Purpose:  Searches mounted filesystems for java binary
-# files and displays the location and version of the file.
+# Purpose:  Searches mounted filesystems for java binary files
+# and displays the location and version of the found files.
 # With inspiration from:
 # https://redhatlinux.guru/2016/06/13/shell-script-to-locate-and-check-the-version-of-all-java-binaries/
+#
+# Written by farid Joubbi 2021
 #########################################################################################################
  
 echo "---------------------------------------------------------------"
-echo "Java Binary Report"
+echo "                   -== Java Binary Report ==-                  "
 echo "Hostname:  $(hostname)"
 echo "Date:      $(date)"
 echo "---------------------------------------------------------------"
 
 if file=$(command -v java) ; then
   echo "File location: ${file}"
-  echo "---------------------------------------------------------------"
   "${file}" -version | bash
   echo "---------------------------------------------------------------"
 fi
@@ -26,7 +27,6 @@ for fs in $(mount -v | grep -E -v "nfs|oracle|tmpfs|lofs|ctfs|objfs|fd|devfs|mnt
     # If file is a binary file it executes it with the -version flag and produces an report
     if file "${file}" | grep "executable" &>/dev/null; then
       echo "File Location:  ${file}"
-      echo "---------------------------------------------------------------"
       "${file}" -version | bash
       echo "---------------------------------------------------------------"
     fi
